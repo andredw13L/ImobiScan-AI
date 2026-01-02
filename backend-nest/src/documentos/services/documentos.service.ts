fa9cdd2cf8003ad3d6a743f808a275b213658e82
andredw13L <andreribeiro13l@gmail.com>
2026-01-02 00:21:51 -0300

    feat(fila: implementar envio de documentos para a fila de processamento ocr

--- backend-nest/src/documentos/services/documentos.service.ts
+++ backend-nest/src/documentos/services/documentos.service.ts
@@ -1,79 +1,47 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import FormData from 'form-data';
-import { HttpService } from '@nestjs/axios';
-import { lastValueFrom } from 'rxjs';
-import fs from 'fs';
 
 import { DocumentosEntity } from '../entities/documento.entity';
-
-interface OcrResponse {
-  texto: string;
-  metadados: {
-    valor_contrato: string | null;
-    cpf_encontrado: string | null;
-    data_contrato: string | null;
-  };
-  tipo: string;
-  status: string;
-}
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
 
 @Injectable()
 export class DocumentosService {
   constructor(
     @InjectRepository(DocumentosEntity)
     private readonly docRepository: Repository<DocumentosEntity>,
-    private readonly httpService: HttpService,
+    @InjectQueue('ocr-queue') private readonly ocrQueue: Queue,
   ) {}
 
-  async enviarParaAnalise(file: Express.Multer.File): Promise<OcrResponse> {
-    const formData = new FormData();
-
-    formData.append('file', fs.createReadStream(file.path), {
-      filename: file.originalname,
-      contentType: file.mimetype,
-    });
-
-    try {
-      const response = await lastValueFrom(
-        this.httpService.post<OcrResponse>(
-          'http://ocr-service:8000/extract-text',
-          formData,
-          {
-            headers: { ...formData.getHeaders() },
-            timeout: 60000,
-          },
-        ),
-      );
-      return response.data;
-    } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'Erro desconhecido';
-      console.error('Erro na integração com OCR:', errorMessage);
-      throw new InternalServerErrorException(
-        'O OCR não respondeu corretamente',
-      );
-    }
-  }
-
   async criarComAnalise(file: Express.Multer.File): Promise<DocumentosEntity> {
-    const resultadoOcr = await this.enviarParaAnalise(file);
-
     const novoDocumento = this.docRepository.create({
       nomeOriginal: file.originalname,
       nomeArquivo: file.filename,
       tipo: file.mimetype,
+      path: file.path,
       tamanho: file.size,
       isActive: true,
       analise: {
-        statusProcessamento: 'concluido',
-        textoExtraido: resultadoOcr.texto,
-        metadados: resultadoOcr.metadados,
+        statusProcessamento: 'pendente',
       },
     });
 
-    return await this.docRepository.save(novoDocumento);
+    const docSalvo = await this.docRepository.save(novoDocumento);
+
+    await this.ocrQueue.add(
+      'process-ocr',
+      {
+        documentId: docSalvo.id,
+        filePath: docSalvo.path,
+      },
+      {
+        attempts: 3,
+        backoff: 2000,
+      },
+    );
+
+    return docSalvo;
   }
 
   async listarTodos(): Promise<DocumentosEntity[]> {
